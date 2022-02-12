0f31703d21ff49bbf99f71933a173bf7ef865da9
ahDevGit <justalihaider@gmail.com>
2022-02-12 16:41:15 +0600

    Dashboard Page Functionality Implement

---
+++ js/dashboard.js
@@ -0,0 +1,40 @@
+/* Deposit Balance */
+document.getElementById('btn-deposit').addEventListener('click', function () {
+  // Get value
+  const targetInput = document.getElementById('deposit-input');
+  const inputValue = targetInput.value;
+  const convertValue = parseFloat(inputValue);
+
+  // Set Value
+  const history = document.getElementById('deposit-balance');
+  const historyValue = history.innerText;
+  const convertHistoryValue = parseFloat(historyValue);
+  history.innerText = convertValue + convertHistoryValue;
+
+  // Update Total Balance
+  const totalBalance = document.getElementById('total-balance');
+  const totalBalanceValue = totalBalance.innerText;
+  const convertTotalBalance = parseFloat(totalBalanceValue);
+  totalBalance.innerText = convertTotalBalance + convertValue;
+})
+
+/* Withdraw Balance */
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+  // Set Value
+  const targetInput = document.getElementById('withdraw-input');
+  const inputValue = targetInput.value;
+  const convertValue = parseFloat(inputValue);
+
+  // Get Value
+  const history = document.getElementById('withdraw-balance');
+  const historyValue = history.innerText;
+  const convertHistoryValue = parseFloat(historyValue);
+  history.innerText = convertValue + convertHistoryValue;
+
+  // Update Total Balance
+  const totalBalance = document.getElementById('total-balance');
+  const totalBalanceValue = totalBalance.innerText;
+  const convertTotalBalance = parseFloat(totalBalanceValue);
+  totalBalance.innerText = convertTotalBalance - convertValue;
+})
+
